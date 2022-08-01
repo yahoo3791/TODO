3ef42b1ca68e2fdf3e544e614f56d4a0eb3a84c4
JIE <youco1327@yahoo.com.tw>
2022-08-02 00:08:46 +0800

    update

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ let txt = document.querySelector('.txt');
 let btn = document.querySelector('.btn');
 let list = document.querySelector('.todolist');
 //取data parse轉js object  getItem取出todoName(keycode) or []
-let data = JSON.parse(localStorage.getItem("todoName"))||[];
+let data = JSON.parse(localStorage.getItem("todoName")) || [];
+let txtValue = txt.value;
 
 //btn 按鈕監聽
-btn.addEventListener('click',function(e){
-  let txtValue = txt.value;
-  if (txtValue == ''){
+btn.addEventListener('click', function (e) {
+  if (txtValue == '' || txtValue == undefined || txtValue == null) {
     alert('請輸入文字');
     return;
   }
   let todo = {
-    todotxt : txtValue
+    todotxt: txtValue
   }
   data.push(todo);
   updateList(data);
@@ -22,28 +22,27 @@ btn.addEventListener('click',function(e){
 //txt enter鍵監聽
 txt.addEventListener('keypress', function (e) {
   let txtValue = txt.value;
-  if(e.keyCode == 13 ){
-    if(txtValue == ''){
+  if (e.keyCode == 13) {
+    if (txtValue == '') {
       alert('請輸入文字');
       return;
     }
-  let todo = {
-    todotxt: txtValue
-  }
-  data.push(todo);
-  updateList(data);
-  localStorage.setItem("todoName", JSON.stringify(data));
-  txt.value = '';
+    let todo = {
+      todotxt: txtValue
+    }
+    data.push(todo);
+    updateList(data);
+    localStorage.setItem("todoName", JSON.stringify(data));
+    txt.value = '';
   }
 });
 //資料更新累加顯示
-function updateList(data){
+function updateList(data) {
   let str = '';
   let total = data.length;
-  for( let i = 0; i < total; i++){
+  for (let i = 0; i < total; i++) {
     str += `<li class="todo">
-        <input type="checkbox" style="width: 30px; height: 30px;">
-        <p class="todo_p">${data[i].todotxt}</p>
+        <p class="todo_p">${i + 1}. ${data[i].todotxt}</p>
         <a href='#' data-num=${i} class="deletBtn">
           <i class="bi bi-trash" style="font-size: 26px;"></i>
         </a>
@@ -54,11 +53,11 @@ function updateList(data){
 updateList(data);
 
 //刪除某筆dataset資料
-list.addEventListener('click',e => {
-  if (e.target.getAttribute('class') == "deletBtn"){
+list.addEventListener('click', e => {
+  if (e.target.getAttribute('class') == "deletBtn") {
     let num = e.target.getAttribute('data-num');
-    data.splice(num,1);
-    localStorage.setItem("todoName",JSON.stringify(data));
+    data.splice(num, 1);
+    localStorage.setItem("todoName", JSON.stringify(data));
     updateList(data);
   }
 })
@@ -66,11 +65,11 @@ list.addEventListener('click',e => {
 //刪除全部 按鈕監聽和事件
 let deleteAll = document.querySelector('.deleteAll');
 deleteAll.addEventListener('click', (e) => {
-  if (e.target.nodeName == "BUTTON"){
+  if (e.target.nodeName == "BUTTON") {
     localStorage.clear();
-    data=[];
-    localStorage.setItem("todoName",JSON.stringify(data));
+    data = [];
+    localStorage.setItem("todoName", JSON.stringify(data));
     updateList(data);
-    // location.reload();
+    // location.reload(); codepen不能用 改用setItem
   }
 })
